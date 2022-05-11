bef7e8009f386ac5972cc981e88e257e42debe4a
Ori Azualy <ori1019@hotmail.com>
2022-05-11 16:28:57 +0300

    controller update

---
+++ WebServer/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer.Models
+{
+    public class Login
+    {
+        public string LoginProvider { get; set; }
+        public string Provider { get; set; }
+        public bool RememberMe { get; set; }
+        
+    }
+}
