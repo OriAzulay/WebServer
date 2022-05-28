d332b07aaa2907bf0fc479636992c826f311deb8
KareenSalameh <itskareen3@hotmail.com>
2022-05-28 21:45:27 +0300

    Last Update for Post

--- WebServer/Controllers/contactsController.cs
+++ WebServer/Controllers/contactsController.cs
@@ -24,11 +24,6 @@ namespace WebServer.Controllers
 
         [HttpGet]
 
-        // GET: Users
-       /* public async Task<IActionResult> Index()
-        {
-              return Json(await _context.User.ToListAsync());
-        }*/
         public async Task<IActionResult> Index2()
         {
             var q = _context.Contacts;
@@ -50,7 +45,88 @@ namespace WebServer.Controllers
             }
             return Json(c);
 
-        }   
+        }
+     
+        [HttpDelete("{id}")]
+        //api/contacts/id
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var c = await _context.Contacts.FindAsync(id);
+            if (c == null)
+            {
+                return StatusCode(204);
+            }
+            if (c.Id == id)
+            {
+                 _context.Remove(c);
+                 return Ok();
+            }
+            
+            _context.Contacts.Remove(c);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Contact c)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _context.Add(c);
+                await _context.SaveChangesAsync();
+                return Created(string.Format("api/contacts/{0}", c.Id), c);
+
+            }
+            return BadRequest();
+        }
+
+        public class addcontact
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Server { get; set; }
+            public string Connected { get; set; }
+        }
+       /*
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] addcontact cont /*string connected,  string id, string name, string server*/ /*[Bind("Id,Name,Server")] Contacts contacts*/
+/*{
+
+            Contact contact = new Contact() { Id = cont.Id, Server = cont.Server, Name = cont.Name };
+            if (contact == null)
+            {
+                return BadRequest();
+            }
+            contact.LastDate = null;
+            contact.Last = null;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        */
+
+     
+        /* private dynamic addContact(Contact contact)
+{
+    if (contact == null)
+    {
+        return (new { });
+    }
+    var temp = new
+    {
+        id = contact.Id,
+        name = contact.Name,
+        server = contact.Server,
+        last = contact.Last,
+        lastDate = contact.LastDate
+    };
+    return temp;
+}*/
+
 
 
     }
---
+++ WebServer/wwwroot/js/rest.js
@@ -0,0 +1,18 @@
+﻿async function getAll() {
+    const r = await fetch('/api/contacts');
+    const d = await r.json();
+    console.log(d);
+}
+
+async function post() {
+    const r = await fetch('/api/contacts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: 'dav', name: 'David', server: 'localhost:1234', last: 'Bye' })
+       
+    });
+    //const d = await r.json();
+    console.log(r);
+}
